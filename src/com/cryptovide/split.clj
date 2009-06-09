8bb9c4ca395df692dc32e5e6746770823491994e
Arthur Ulfeldt <arthur@ulfeldt.com>
2009-06-08 17:15:17 -0700

    added bad prng calls and fixed assiciated bugs. fixed bug: pass the same prng seq to all splitter tasks

--- src/com/cryptovide/split.clj
+++ src/com/cryptovide/split.clj
@@ -8,13 +8,15 @@
    com.cryptovide.misc))
 
 (def coificients (cycle [1 2 3 4 5 6 7 8 ]))
-(defn get-prng [] coificients)
+(defn get-prng [] (rand-seq mody))
+;(defn get-prng [] coificients)
 
 (defn split [secrets threshold parts]
   "lazy seqs of the [x (secrets applied to a polynomial of threshold degree )]"
   (let [ spliter (fn [x coificients]
                    (map #(eval-poly (cons %1 %2) x)
-                     secrets (partition (dec threshold) (get-prng))))]
-    (map (fn [x] (struct secret x 1 (spliter x coificients )))
+                     secrets (partition (dec threshold) coificients)))
+        prng (get-prng)]
+    (map (fn [x] (struct secret x 1 (spliter x prng)))
       (take parts (range 1 (inc parts))))))
 
