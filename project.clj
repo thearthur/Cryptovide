ea1813173f1c0e0abf52af6320ed5d0a9a6f5db0
Arthur Ulfeldt <arthur@ulfeldt.com>
2010-01-28 02:47:53 -0800

    add leiningen config file

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.cryptovide "0.1"
+    :dependencies [[org.clojure/clojure
+                      "1.1.0-master-SNAPSHOT"]
+                   [org.clojure/clojure-contrib
+                      "1.0-SNAPSHOT"]]
+    :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+    :main com.cryptovide.main)
