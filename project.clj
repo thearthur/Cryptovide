a32a09e59e24ad5d4704a7ad3ea78abc0fa5a2e1
Arthur Ulfeldt <arthur@ulfeldt.com>
2010-09-06 11:09:05 -0700

    add swank-clojure build dep.
    switching leiningen/lein-swank for swank-clojure to work with cake instead of leiningen

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject cryptovide "0.1"
-    :dependencies [[org.clojure/clojure "1.1.0"]
-                   [org.clojure/clojure-contrib "1.1.0"]
+    :dependencies [[org.clojure/clojure "1.2.0"]
+                   [org.clojure/clojure-contrib "1.2.0"]
                    [log4j "1.2.15" :exclusions [javax.mail/mail
                                                 javax.jms/jms
                                                 com.sun.jdmk/jmxtools
                                                 com.sun.jmx/jmxri]]]
-    :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+;    :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+    :dev-dependencies [[swank-clojure "1.2.1"]]
     :main com.cryptovide.main)
